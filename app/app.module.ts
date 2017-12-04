a359ae1984a46c4830e11ac77d22b4e7f64ec0ae
Casey Dailey <casey@feasttogether.org>
2017-12-04 16:02:22 -0600

    bootstrapping the appModule

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
-@ngModule({
+@NgModule({
   imports: [
     BrowserModule
   ],
-  declaration: [
+  declarations: [
     AppComponent
   ],
   bootstrap: [
